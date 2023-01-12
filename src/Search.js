eac649d4345799681711b574a195b9539ac3a172
MarkXtenda <mark.i2p.1998@gmail.com>
2023-01-12 18:37:51 +1100

    3. Adding condition logic to 'Search' component

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,18 @@
 function Search({search, rating}) {
-  
-    return (
-        <section>
-            Search
-            <p>{search}</p>
-            <p>{rating}</p>
-        </section>
-    );
+    if (search.length > 0) {
+        return (
+            <section>
+                Search
+                <p>{search}</p>
+                <p>{rating}</p>
+            </section>
+        );
+    } else {
+        return (
+            <section>
+            </section>
+        );
+    }
   }
   
   export default Search;
