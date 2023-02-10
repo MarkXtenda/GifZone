3d3f6f35275b9b43324b0212642ceb5a5cd36896
MarkXtenda <mark.i2p.1998@gmail.com>
2023-02-10 14:51:39 +1100

    13. Copyright has been added.

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -2,9 +2,9 @@ function HeroSection({display}) {
     return(
     <section id="hero">
         <h1>Welcome to My Giph Search Website</h1>
-        <p>You can discover or search for giphs using search bar above.
-        <br/>This window will disappear whenever you search for something or click dismiss.
-        <br/>This website was created by Mark Kholodii</p>
+        <p className="hero-content">You can discover or search for giphs using search bar above.
+        <br/>This window will disappear whenever you search for something or click dismiss.</p>
+        <p id="copyright">Created by  &copy; Mark Kholodii, 2022</p>
         <div>
         <a href="https://github.com/MarkXtenda/GiphsWeb-React-App">Learn More</a>
         <a href="#" onClick={()=>display(false)}>Dismiss</a>
